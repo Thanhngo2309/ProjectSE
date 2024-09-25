a343fd1a6a267149cd8e524a49e6de7fc09ac780
Thanhngo2309 <kamenriderlc@email.com>
2024-09-25 22:25:15 +0700

    add asset and exploreMenu

---
+++ QLBIA/src/assets/assets.ts
@@ -0,0 +1,54 @@
+import menu_1 from "./club_ParadiseBilliardsClub.png"
+import menu_2 from "./clubBilliardsCoffeeT&T.png"
+import menu_3 from "./MarvelBilliardClub.png"
+import menu_4 from './BeanBilliardsClub.png'
+import menu_5 from './X3BilliardsClub.png'
+import menu_6 from './CanadaBilliardsClub.png'
+import menu_7 from './BilliardsKinhTe.png'
+import menu_8 from './VTCBilliardClub.png'
+import menu_9 from './BilliardsKingKong.png'
+import menu_10 from './TheOneBilliardClub.png'
+import menu_11 from './BananaBilliardsClub.png'
+import menu_12 from './99BilliardsClub.png'
+import menu_13 from './ThongPhongBilliard Club.png'
+export const menu_list = [
+    {club_name: "Paradise Billiards Club",
+     club_image: menu_1
+    },
+    {club_name: "Club Billiards Coffee T&T",
+     club_image: menu_2
+    },
+    {club_name: "Marvel Billiard Club",
+     club_image: menu_3
+    },
+    {club_name: "Bean Billiards Club ",
+     club_image: menu_4
+    },
+    {club_name: "X3 Billiards Club",
+     club_image: menu_5
+    },
+    {club_name: "Canada Billiards Club",
+     club_image: menu_6
+    },
+    {club_name: "Billiards Kinh Tế",
+    club_image: menu_7
+    },
+    {club_name: "VTC Billiard Club",
+    club_image: menu_8
+    },
+    {club_name: "Billiards KingKong",
+    club_image: menu_9
+    },
+    {club_name: "The One Billiard Club",
+    club_image: menu_10
+    },
+    {club_name: "Banana Billiards Club",
+    club_image: menu_11
+    },
+    {club_name: "99 Billiards Club",
+    club_image: menu_12
+    },
+    {club_name: "Thông Phong Billiard Club",
+    club_image: menu_13
+    },
+]
--- QLBIA/src/pages/Home/HomeScreen.tsx
+++ QLBIA/src/pages/Home/HomeScreen.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeScreem.css";
 import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu.tsx/ExploreMenu";
 
 const HomeScreen = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 };
